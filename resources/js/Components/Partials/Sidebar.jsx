2776416891e00bb53525ea62586668feac67688d
Esau21 <esaumoran1999@gmail.com>
2025-05-10 21:57:18 -0600

    commit de inventarios

--- resources/js/Components/Partials/Sidebar.jsx
+++ resources/js/Components/Partials/Sidebar.jsx
@@ -132,6 +132,13 @@ function Sidebar() {
                                             )}
                                         </li>
                                         <li>
+                                            {can('compras_view') && (
+                                                <Link href={route('compras.index')}>
+                                                    <span className="sub-item">Ordenes de compra</span>
+                                                </Link>
+                                            )}
+                                        </li>
+                                        <li>
                                             {can('productos_view') && (
                                                 <Link href={route('producto.index')}><span className="sub-item">Productos</span></Link>
                                             )}
